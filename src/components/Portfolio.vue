58af81e64bb5a86a9ca869bbe0b7665d9040f6fd
Krzysiek <krzysztofu1024@gmail.com>
2026-02-02 19:29:01 +0100

    refactor: Improve project structure

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -13,7 +13,11 @@
       >
         <div class="portfolio__card">
           <div class="portfolio__image-wrapper">
-            <img :src="project.image" alt="" loading="lazy" />
+            <img
+              :src="project.image"
+              :alt="'Zrzut ekranu projektu ' + project.title"
+              loading="lazy"
+            />
           </div>
           <div class="portfolio__card-body">
             <div>
@@ -21,7 +25,7 @@
               <p class="portfolio__card-description font-p">{{ project.description }}</p>
               <div class="portfolio__tech-stack">
                 <span
-                  v-for="(tech, index) in project.technology.split(' / ')"
+                  v-for="(tech, index) in project.technologies"
                   :key="index"
                   class="portfolio__tech-badge"
                 >
@@ -41,7 +45,7 @@
                 <img
                   class="portfolio__icon portfolio__icon--github"
                   src="../assets/img/icons/github.svg"
-                  alt=""
+                  alt="GitHub"
                 />
               </a>
               <a
@@ -63,65 +67,14 @@
 </template>
 
 <script>
+import rawProjectsData from '@/data/projects.js'
 import { addIds } from '@/utils/dataUtils'
-
-const images = import.meta.glob('@/assets/img/*.jpg', { eager: true })
 
 export default {
   name: 'PortfolioComponent',
 
   setup() {
-    const rawProjects = [
-      {
-        title: 'canvico-editor',
-        description:
-          'Modularny edytor obrazów oparty na TypeScript i Canvas API, zaprojektowany jako elastyczna biblioteka do integracji z dowolnym UI. Projekt koncentruje się na pracy z Canvas API, zarządzaniu stanem narzędzi oraz architekturze umożliwiającej swobodne rozmieszczenie elementów interfejsu w strukturze HTML.',
-        github: 'https://github.com/GalacticByte/canvico-editor',
-        live: 'https://galacticbyte.github.io/canvico-editor/demo/',
-        technology: 'TYPESCRIPT / CANVAS API',
-        image: images['/src/assets/img/canvico-editor.jpg'].default,
-      },
-      {
-        title: 'Aplikacja Czat',
-        description:
-          'Aplikacja czatu w czasie rzeczywistym zbudowana w Vue 3 i TypeScript, wykorzystująca Socket.IO oraz własny backend w Express.js. Projekt obejmuje komunikację real-time, prosty mechanizm identyfikacji użytkownika oparty na JWT, persystencję wiadomości w PostgreSQL oraz wymianę danych opartą na DTO jako kontrakcie między frontendem i backendem.',
-        github: 'https://github.com/GalacticByte/Simple-Vue-Chat',
-        live: 'https://chat-app-lh2l.onrender.com/',
-        technology:
-          'VUE / TAILWIND CSS / TYPESCRIPT / VITE / SOCKET.IO / NODE.JS / EXPRESS.JS / PRISMA / POSTGRESQL / JWT',
-        image: images['/src/assets/img/chat-app-02.jpg'].default,
-      },
-      {
-        title: 'ecovaro-corporate-site ',
-        description:
-          'Fikcyjna strona korporacyjna oparta na autorskim motywie WordPress, zaprojektowana w oparciu o modułową architekturę sekcji. Projekt obejmuje dynamiczne formularze oraz elastyczny system pól oparty na Carbon Fields jako alternatywę dla ACF Pro.',
-        github: 'https://github.com/GalacticByte/ecovaro-corporate-site',
-        live: 'https://ecovaro-demo-website.42web.io/ecovaro/',
-        technology: 'WORDPRESS / BOOTSTRAP / PHP / jQuery / CARBON FIELDS',
-        image: images['/src/assets/img/ecovaro-corporate-site-01.jpg'].default,
-      },
-      {
-        title: 'Notatnik',
-        description:
-          'Aplikacja do zarządzania notatkami w formacie Markdown, zbudowana w React i TypeScript. Umożliwia dodawanie, edytowanie i usuwanie notatek, edycję z podglądem na żywo, wyszukiwanie w czasie rzeczywistym oraz eksport do PDF. Stan serwerowy zarządzany jest przy użyciu TanStack Query, a backend oparto na Express.js i MongoDB, z kontraktowym API OpenAPI 3.0 i walidacją danych w runtime.',
-        github: 'https://github.com/GalacticByte/notes-app',
-        live: 'https://notes-app-k7v6.onrender.com/',
-        technology:
-          'REACT / TYPESCRIPT / VITE / TANSTACK QUERY / NODE.JS / EXPRESS.JS / MONGODB / OPENAPI',
-        image: images['/src/assets/img/notes-app-screenshot.jpg'].default,
-      },
-      {
-        title: 'Strona Wizytówka',
-        description:
-          'Strona portfolio zbudowana w Vue, zaprojektowana jako prosta aplikacja prezentacyjna do wyświetlania projektów i informacji o autorze. Projekt opiera się na czytelnej strukturze aplikacji oraz komponentach dopasowanych do poszczególnych sekcji strony.',
-        github: 'https://github.com/GalacticByte/Portfolio-page',
-        live: 'https://galacticbyte.github.io/Portfolio-page/',
-        technology: 'VUE / VITE / BOOTSTRAP / SCSS',
-        image: images['/src/assets/img/portfolio-page-02.jpg'].default,
-      },
-    ]
-
-    const projects = addIds(rawProjects)
+    const projects = addIds(rawProjectsData)
 
     return { projects }
   },
--- src/components/Technologies.vue
+++ src/components/Technologies.vue
@@ -1,5 +1,9 @@
 <template>
-  <section id="technologies" class="px-2 technologies" aria-labelledby="technologies-heading">
+  <section
+    id="technologies"
+    class="px-2 px-md-3 technologies"
+    aria-labelledby="technologies-heading"
+  >
     <div class="row g-0 text-center technologies__header">
       <div class="col-12">
         <h2
@@ -26,7 +30,7 @@
           >
             <div class="technologies__item d-flex flex-column align-items-center h-100">
               <span v-if="tech.tooltip" class="technologies__highlight-label"></span>
-              <img :src="tech.image" class="technologies__image" alt="" />
+              <img :src="tech.image" class="technologies__image" :alt="'Logo ' + tech.title" />
               <div class="technologies__info mt-auto">
                 <p class="technologies__name font-p mb-0">{{ tech.title }}</p>
               </div>
@@ -39,120 +43,16 @@
 </template>
 
 <script>
+import rawTechsData from '@/data/technologies.js'
 import { addIds } from '@/utils/dataUtils'
-
-const images = import.meta.glob('@/assets/img/icons/*.svg', { eager: true })
 
 export default {
   name: 'TechnologiesComponent',
 
   setup() {
-    const rawTechs = {
-      Frontend: [
-        {
-          title: 'HTML',
-          image: images['/src/assets/img/icons/html-icon.svg'].default,
-        },
-        {
-          title: 'CSS',
-          image: images['/src/assets/img/icons/css-file-format.svg'].default,
-        },
-        {
-          title: 'Sass',
-          image: images['/src/assets/img/icons/sass-icon.svg'].default,
-        },
-        {
-          title: 'Bootstrap',
-          image: images['/src/assets/img/icons/bootstrap5-icon.svg'].default,
-        },
-        {
-          title: 'RWD',
-          image: images['/src/assets/img/icons/phone-tablet-and-laptop.svg'].default,
-        },
-        {
-          title: 'JS',
-          image: images['/src/assets/img/icons/js-icon.svg'].default,
-        },
-        {
-          title: 'jQuery',
-          image: images['/src/assets/img/icons/jquery-icon.svg'].default,
-        },
-        {
-          title: 'Tailwind CSS',
-          image: images['/src/assets/img/icons/tailwind-icon.svg'].default,
-        },
-        {
-          title: 'Vue.js',
-          image: images['/src/assets/img/icons/vue-icon.svg'].default,
-        },
-        {
-          title: 'React',
-          image: images['/src/assets/img/icons/react.svg'].default,
-        },
-        {
-          title: 'Typescript',
-          image: images['/src/assets/img/icons/typescript-logo.svg'].default,
-        },
-      ],
-      'Narzędzia i Bundlery': [
-        {
-          title: 'Vite',
-          image: images['/src/assets/img/icons/vitejs-logo.svg'].default,
-        },
-        {
-          title: 'Webpack',
-          image: images['/src/assets/img/icons/webpack-icon.svg'].default,
-        },
-        {
-          title: 'Gulp',
-          image: images['/src/assets/img/icons/gulp-icon.svg'].default,
-        },
-      ],
-      'Backend i środowiska uruchomieniowe': [
-        {
-          title: 'Node.js',
-          image: images['/src/assets/img/icons/nodejs-icon.svg'].default,
-        },
-      ],
-      CMS: [
-        {
-          title: 'WordPress',
-          image: images['/src/assets/img/icons/wordpress.svg'].default,
-        },
-      ],
-      'Chmura i Integracje': [
-        {
-          title: 'Cloudflare',
-          image: images['/src/assets/img/icons/cloudflare-icon.svg'].default,
-        },
-        {
-          title: 'Workers',
-          image: images['/src/assets/img/icons/cloudflare-workers-icon.svg'].default,
-        },
-        {
-          title: 'Google Apps Script',
-          image: images['/src/assets/img/icons/google-apps-script-icon.svg'].default,
-        },
-      ],
-      'Kontrola wersji': [
-        {
-          title: 'GIT',
-          image: images['/src/assets/img/icons/git-icon.svg'].default,
-        },
-        {
-          title: 'Gitlab',
-          image: images['/src/assets/img/icons/gitlab-icon2.svg'].default,
-        },
-        {
-          title: 'Github',
-          image: images['/src/assets/img/icons/github-icon.svg'].default,
-        },
-      ],
-    }
-
     let idCounter = 1
     const categorizedTechs = Object.fromEntries(
-      Object.entries(rawTechs).map(([category, techs]) => {
+      Object.entries(rawTechsData).map(([category, techs]) => {
         const techsWithIds = addIds(techs, idCounter)
         idCounter += techsWithIds.length
         return [category, techsWithIds]
